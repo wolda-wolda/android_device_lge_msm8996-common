970f4597d9c6e9b4df40f8084e2a7c0c4fad55fa
Michael Bardoň <wolfovelp@gmail.com>
2021-01-15 21:57:49 +0100

    msm8996-common: More EAS removal

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -12,19 +12,11 @@ LOCAL_VENDOR_MODULE    := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.power.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := bin/init.power.sh
-LOCAL_VENDOR_MODULE    := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
-LOCAL_REQUIRED_MODULES := init.power.sh init.qcom.graphics.sh
+LOCAL_REQUIRED_MODULES := init.qcom.graphics.sh
 LOCAL_VENDOR_MODULE    := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)

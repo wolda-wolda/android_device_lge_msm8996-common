95d18be3e3df460294b753348ae95f4e67db1170
Michael Bardoň <wolfovelp@gmail.com>
2021-02-27 19:00:54 +0100

    msm8996-common: Remove pdfium

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -168,9 +168,6 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USES_MKE2FS := true
 BOARD_ROOT_EXTRA_FOLDERS := firmware persist persist-lg sns mpt
 
-# pdfium
-TARGET_NEEDS_PDFIUM_BIGINT := true
-
 # Power
 TARGET_HAS_NO_WLAN_STATS := true
 
